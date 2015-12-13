7e30ee999e074339f951a50bd9352dcf8e03cae4
jordanpohl <jordan.pohl@gmail.com>
2015-12-13 16:14:56 -0500

    Update plot_3.R

--- plot_3.R
+++ plot_3.R
@@ -1,1 +1,20 @@
+source("load_data.R")
 
+plot_3 <- function(data=NULL) {
+    if(is.null(data))
+        data <- load_data()
+
+    png("plot3.png", width=480, height=480)
+
+    plot(data$Time, data$Sub_metering_1, type="l", col="black",
+         xlab="", ylab="Energy sub metering")
+    lines(data$Time, data$Sub_metering_2, col="red")
+    lines(data$Time, data$Sub_metering_3, col="blue")
+    legend("topright",
+           col=c("black", "red", "blue"),
+           c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+           lty=1)
+
+    dev.off()
+cat("plot3.png has been saved in", getwd())
+}
