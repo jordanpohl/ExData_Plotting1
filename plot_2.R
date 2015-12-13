e392a5ce0662b4edd681b6583b5a9f85de643f98
jordanpohl <jordan.pohl@gmail.com>
2015-12-13 15:31:57 -0500

    Update plot_2.R

--- plot_2.R
+++ plot_2.R
@@ -1,1 +1,7 @@
-
+plot_2 <- function() {
+        plot(data$Time,data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+        dev.copy(png, file="plot_2.png", width=480, height=480)
+        dev.off()
+        cat("plot_2.png has been saved in", getwd())
+}
+plot_2()
