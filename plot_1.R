5da666f5d21f6bb3fa433a0409087087f906b90f
jordanpohl <jordan.pohl@gmail.com>
2015-12-13 12:22:46 -0500

    Create plot_1.R

---
+++ plot_1.R
@@ -0,0 +1,6 @@
+plot_1 <- function(){
+            hist(data$Global_active_power, main= paste("Global Active Power"), col = "red", xlab = "Global Active Power(kilowatts)")
+            dev.copy(png, file="png1.png", width=480, height=480)
+            dev.off()
+            cat("Plot1.png has been saved in", getwd())}
+plot_1()
